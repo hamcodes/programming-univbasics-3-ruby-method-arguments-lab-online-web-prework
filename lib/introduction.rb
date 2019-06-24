6b2312c872a4d4aafc0483d812386ec01429232c
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-24 18:43:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,7 @@ def introduction(name)
   "Hi, my name is #{name}."
 end
 puts Hi, my name is (name)
+def introduction_with_language
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+puts Hi, my name is 
