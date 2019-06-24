d46627a19526beaae846afc5b7a42aa21c41db18
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-24 18:48:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,6 @@ def introduction_with_language
 end
 puts Hi, my name is (name) and I am learning to program in (language).
 def introduction_with_language_optional
-    "Hi, my name is #{name} and I am learning to program in #{Ruby}."
+    "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-puts Hi, my name is (name) and I am learning to program in (language).
+puts Hi, my name is (name) and I am learning to program in (Ruby).
